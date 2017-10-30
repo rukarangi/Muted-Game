499646c99d4985933c8f825cecde29cf07a8c0ff
rukarangi <luke@yorkbay.net.nz>
2017-10-30 15:33:20 +1300

    Main Updates

--- Main.elm
+++ Main.elm
@@ -4,6 +4,8 @@ import Time exposing (..)
 import Platform exposing (..)
 import Window
 import Task
+import Random
+import Array exposing (..)
 import Html exposing (Html)
 import Svg exposing (..)
 import Svg.Events exposing (onClick)
@@ -20,8 +22,8 @@ main = Html.program
 
 subscriptions : Model -> Sub Msg
 subscriptions model = Sub.batch 
-    [ Time.every  (200 * millisecond) Tick
-    , Keyboard.downs DownsInfo
+    [ Time.every (0.5 * millisecond) Tick
+    , Keyboard.presses DownsInfo
     ]
 
 --Veiw
@@ -35,20 +37,32 @@ view model =
 
 menuView : Model -> Html Msg
 menuView model =
-    let w = toString model.gameWidth
-        h = toString model.gameHeight
+    let w = model.gameWidth
+        h = model.gameHeight
+        buttonX = (toFloat w) / 2 - 193
+        buttonY = (toFloat h) / 2 - 65
     in svg
-      [ width w, height h, viewBox ("0 0 " ++ w ++ " " ++ h ++ "")]
-      [ image [ xlinkHref "/static/buttonPlaceholder.jpg", width "386", height "131", x "300", y "300"][]
+      [ width (toString w), height (toString h), viewBox ("0 0 " ++ (toString w) ++ " " ++ (toString h) ++ "")]
+      [ image [ xlinkHref "static/buttonPlaceholder.jpg"
+      , width "386", height "131"
+      , x (toString buttonX)
+      , y (toString (buttonY + 200))
+      , Svg.Events.onClick StatePlay]
+      []
       ]
 
 playView : Model -> Html Msg
 playView model = 
-    let w = toString model.gameWidth
-        h = toString model.gameHeight
+    let enemie = Array.forEach enemieRender model.ai
+        w = model.gameWidth
+        h = model.gameHeight
+        platformX = (toFloat w) / 2
+        platformY = (toFloat h) / 2
     in svg
-      [ width w, height h, viewBox ("0 0 " ++ w ++ " " ++ h ++ "")]
-      [ image [ xlinkHref "/static/Final Muted hero.png", width "386", height "131", x (toString model.obi.x), y (toString model.obi.y)][]
+      [ width (toString w), height (toString h), viewBox ("0 0 " ++ (toString w) ++ " " ++ (toString h) ++ "")]
+      [ enemie
+      , image [ xlinkHref "static/Final Muted hero.png", width "100", height "100", x (toString model.obi.x), y (toString model.obi.y)][]
+      , image [ xlinkHref "static/placeholderplatform.png", width "390", height "60", x (toString platformX), y (toString (platformY + 100)) ][]
       ]
 
 overView : Model -> Html Msg
@@ -56,17 +70,18 @@ overView model =
     let w = toString model.gameWidth
         h = toString model.gameHeight
     in svg
-      [ width w, height h, viewBox ("0 0 " ++ w ++ " " ++ h ++ "")]
-      [ image [ xlinkHref "/static/buttonPlaceholder.jpg", width "386", height "131", x "300", y "300"][]
+      [ width (toString w), height (toString h), viewBox ("0 0 " ++ (toString w) ++ " " ++ (toString h) ++ "")]
+      [ image [ xlinkHref "static/buttonPlaceholder.jpg", width "386", height "131", x "300", y "300", Svg.Events.onClick StateMenu][]
+      , image [ xlinkHref "static/buttonPlaceholder.jpg", width "386", height "131", x "300", y "300", Svg.Events.onClick StatePlay][]
       ]
 
---viewms : Model -> Html Msg
+--viewms : Model -> HtmluMsg
 
 walkLeft : Int -> Model -> Model
 walkLeft kc model =
     let obi = model.obi    
-    in  if kc == 65 then     
-            let nobi = { obi | vx = obi.vx - 10 }
+    in  if kc == 97 then     
+            let nobi = { obi | vx = obi.vx - 1.2 }
             in  { model | obi = nobi }
         else
             model
@@ -74,8 +89,8 @@ walkLeft kc model =
 walkRight : Int -> Model -> Model
 walkRight kc model =
     let obi = model.obi    
-    in  if kc == 68 then     
-            let nobi = { obi | vx = obi.vx + 10 }
+    in  if kc == 100 then     
+            let nobi = { obi | vx = obi.vx + 1.2 }
             in  { model | obi = nobi }
         else
             model
@@ -83,87 +98,159 @@ walkRight kc model =
 jump : Int -> Model -> Model
 jump kc model = 
     let obi = model.obi    
-    in  if kc == 87 then
+    in  if kc == 119 then
             if obi.vy == 0 then     
-                let nobi = { obi | vx = obi.vy + 10 }
+                let nobi = { obi | vy = -20 } --Got to figue out how to do jump acceleration! and edge acceleration
                 in  { model | obi = nobi }
             else
                 model
         else
             model
 
-
-
 --Update 
 
-characterUpdate : Bool -> Model -> Character -> Character
-characterUpdate standingUpdate model obi =
-    { obi | x = obi.x + obi.vx
-            , y = obi.y + obi.vy
-            , vy = if standingUpdate == True then
-                        0
+characterUpdate : Character -> Character
+characterUpdate character =
+    let standingUpdate = if character.y >= 600 then
+                                    True
+                                 else
+                                    False
+        edgeUpdate = if character.x < -250 || character.x > 1500 then
+                                    True
+                             else
+                                    False
+        friction = if standingUpdate then
+                        True
                    else
-                        obi.vy + model.gravity
-            , vx = obi.vx }
+                        False
+    in  if character.exist == Exist then 
+            { character | x = character.x + character.vx
+            , y = Basics.min 600 (character.y + character.vy)
+            , vy = if standingUpdate then
+                        Basics.min 0 character.vy
+                   else
+                         character.vy + character.gravity
+            , gravity = if standingUpdate then
+                            0
+                        else 
+                            1
+            , exist = if character.life == 0 then
+                        NonExist
+                      else 
+                        Exist
+            , action = if standingUpdate then
+                        StandingStill
+                       else
+                        Jump
+            , vx = if edgeUpdate then
+                        0
+                    else
+                        if friction then
+                            if character.vx > 0.1 then
+                                character.vx - 0.1
+                            else if character.vx < -0.1 then
+                                character.vx + 0.1
+                            else if character.vx < 0.1 && character.vx > 0 then
+                                character.vx - character.vx
+                            else if character.vx > -0.1 && character.vx < 0 then
+                                character.vx + abs character.vx
+                            else
+                                character.vx
+                        else
+                        character.vx 
+            , area = if character.x < 860 && character.y < 350 then
+                        TopLeft
+                     else if character.x < 860 && character.y > 350 then
+                        BottomLeft
+                     else if character.x > 860 && character.y > 350 then
+                        BottomRight
+                     else if character.x > 860 && character.y < 350 then
+                        TopRight
+                     else
+                        TopLeft}
+        else
+            character
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     case msg of
+        EnemyNo enemies ->
+            let ai = model.ai    
+            in ({ model | ai = Array.map (initiateEnemy enemies) ai}, Cmd.none)
+        StateOver ->   
+            ({ model | state = Over}, Cmd.none)
+        StateMenu ->
+            ({ model | state = Menu}, Cmd.none)
+        StatePlay ->
+            let
+                obi = model.obi
+                nobi = { obi | exist = Exist}
+                nai = model.ai
+            in
+                ({ model | state = Play
+                 , level = 1
+                 , ai = nai
+                 , obi = nobi}
+                 , Cmd.none)
         Tick time -> 
             let obi = model.obi
-                standingUpdate = if obi.y > 400 then
-                                    True
-                                 else
-                                    False
-                nobi = characterUpdate standingUpdate model obi
-            in  ( { model | time = time, obi = nobi }, Cmd.none)
+                ai = model.ai
+                nai = (Array.map (characterUpdate << pathfind obi) ai) 
+                nobi = characterUpdate obi
+            in  ( { model | time = time
+                          , obi = nobi
+                          , ai = nai }
+                          , Random.generate EnemyNo (Random.int 1 4))
         WindowSize size -> ( { model | gameWidth = size.width
                                      , gameHeight = size.height }, Cmd.none)
         DownsInfo kc -> 
             (model |> jump kc |> walkLeft kc |> walkRight kc, Cmd.none)
 
-{-          
-actionCheck : Msg -> Model -> Model
-actionCheck msg model = 
-    msg
-        |> jump msg model 
-        |> walkRight msg model 
-        |> walkLeft msg model 
+pathfind : Character -> Character -> Character
+pathfind obi character =
+    character
 
--}
---Model
+initiateEnemy : Int -> Character -> Character
+initiateEnemy areaNo character =
+    { character | area = if areaNo == 1 then
+                            TopLeft
+                         else if areaNo == 2 then
+                            TopRight
+                         else if areaNo == 3 then
+                            BottomRight
+                         else if areaNo == 4 then
+                            BottomLeft
+                         else
+                            TopLeft
+                , exist = Exist
+                , x = if character.area == TopLeft || character.area == BottomLeft then
+                            0
+                      else if character.area == TopRight || character.area == BottomRight then
+                            1500
+                      else 
+                            0
+                , y = if character.area == TopRight || character.area == TopLeft then
+                            0
+                      else if character.area == BottomRight || character.area == BottomLeft then
+                            575
+                      else
+                            0
+                             }
 
 type alias Character = 
     { x : Float
     , y : Float
     , vx : Float
     , vy : Float
+    , acceleration : Float
     , exist : Existance
     , life : Int
     , standing : Bool
+    , action : Action
+    , gravity : Float
+    , area : Area
     , association : Association
     }  
-
-
-type alias Level =  Int
-
-type Association = Good | Bad
-
-type Existance = NonExist | Exist
-
-type State = Menu | Play | Over
-
-obi : Character
-obi = 
-    { x = 0.00
-    , y = 0.00
-    , vx = 0.00
-    , vy = 0.00
-    , exist = NonExist
-    , life = 5
-    , standing = True
-    , association = Good
-    }
 
 type alias Model = 
     { state : State
@@ -171,25 +258,77 @@ type alias Model =
     , time : Time
     , gameWidth : Int
     , gameHeight : Int
-    , obi : Character 
-    , gravity : Float
+    , ai : Array Character
+    , obi : Character
     }
 
-init : ( Model, Cmd Msg)
+type alias Level =  Int
+
+type Area = TopLeft | TopRight | BottomLeft | BottomRight
+
+type Action = Jump | Walk | StandingStill
+
+type Association = Good | Bad
+
+type Existance = NonExist | Exist
+
+type State = Menu | Play | Over
+
+standardAi : Character
+standardAi = 
+    { x = -30.00
+    , y = -30.00
+    , vx = 0.00
+    , vy = 0.00
+    , acceleration = 0.00
+    , exist = NonExist
+    , life = 5
+    , standing = True
+    , action = StandingStill
+    , gravity = 0.1
+    , area = TopLeft
+    , association = Good
+    }
+
+aiMake : Array Character
+aiMake = 
+    Array.repeat 1 standardAi
+
+obi : Character
+obi = 
+    { x = 0.00
+    , y = 0.00
+    , vx = 0.00
+    , vy = 0.00
+    , acceleration = 0.00
+    , exist = NonExist
+    , life = 5
+    , standing = True
+    , action = StandingStill
+    , gravity = 0.1
+    , area = TopLeft
+    , association = Good
+    }
+
+init : (Model, Cmd Msg)
 init = (
     { state = Menu
     , level = 0
     , time = 0
     , gameWidth = 0
     , gameHeight = 0
+    , ai = aiMake 
     , obi   = obi
-    , gravity = 0.2
     }, Task.perform WindowSize Window.size)
 
 type Msg 
   = Tick Time
   | WindowSize Window.Size
   | DownsInfo Keyboard.KeyCode
+  | StatePlay
+  | StateMenu
+  | StateOver
+  | EnemyNo Int
 
 
 
